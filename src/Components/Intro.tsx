b8b9e2dae89a93843a9c677433cacde6afbe1e64
gcltukjs2022 <gcltukjs2022@gmail.com>
2023-02-12 04:48:31 +0000

    finish basic layout

--- src/Components/Intro.tsx
+++ src/Components/Intro.tsx
@@ -1,20 +1,36 @@
-import React from "react";
+import boomerangImg from "../media/introboomerang.png";
 
 export default function Intro() {
   return (
     <section className="intro">
-      <h1>Your new taxi... for a pound?</h1>
-      <p>
-        Yes, you read that right! Boomerang gives taxi drivers across the UK an
-        opportunity to rent a hybrid vehicle - in line with the nation’s clean
-        air zones - and buy it for just £1 at the end of your term! All of our
-        vehicles comply with the Government’s new environmental strategy, and
-        there’s never been an opportunity to rent a brand new taxi and have the
-        ability to purchase it for less than the price of a takeaway coffee!
-      </p>
-      <a href="/">
-        <button className="btn intro-btn">START NOW</button>
-      </a>
+      <div className="intro-container">
+        <div className="intro-logo">
+          <img src={boomerangImg} alt="boomerang-logo" />
+        </div>
+        <div className="intro-main">
+          <div className="intro-title">
+            <h1>Your new taxi... for a pound?</h1>
+          </div>
+          <div className="intro-paragraph">
+            <p>
+              Yes, you read that right! Boomerang gives taxi drivers across the
+              UK an opportunity to rent a hybrid vehicle - in line with the
+              nation’s clean air zones - and buy it for just £1 at the end of
+              your term!
+              <br />
+              All of our vehicles comply with the Government’s new environmental
+              strategy, and there’s never been an opportunity to rent a brand
+              new taxi and have the ability to purchase it for less than the
+              price of a takeaway coffee!
+            </p>
+          </div>
+          <div className="intro-button">
+            <a href="/">
+              <button className="btn intro-btn">START NOW</button>
+            </a>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
