bbe35df608c969a8f47da1f36c6e65c40c659016
gcltukjs2022 <gcltukjs2022@gmail.com>
2023-02-12 07:10:28 +0000

    created flexbox for benefits

--- src/Components/Benefits.tsx
+++ src/Components/Benefits.tsx
@@ -1,29 +1,42 @@
-import React from "react";
+import clearAirZoneImg from "../media/benefits/cleanairzone.png";
+import roadTaxImg from "../media/benefits/roadtax.png";
+import fuelImg from "../media/benefits/fuel.png";
+import enviornmentImg from "../media/benefits/enviornment.png";
+import chargingImg from "../media/benefits/charging.png";
 
 export default function Benefits() {
   return (
     <section className="benefits">
-      <h1>The benefits of a hybrid taxi</h1>
-      <div className="benefit-container">
-        <div className="benefit1">
+      <div className="benefits-title">
+        <h1>The benefits of a hybrid taxi</h1>
+      </div>
+      <div className="benefits-main">
+        <div className="benefits-item">
+          <img src={clearAirZoneImg} alt="clean-air-zone-icon" />
           <p>Comply with clean air zones - avoid being charged</p>
         </div>
-        <div className="benefit2">
+        <div className="benefits-item">
+          <img src={roadTaxImg} alt="road-tax-icon" />
           <p>Save on road tax</p>
         </div>
-        <div className="benefit3">
+        <div className="benefits-item">
+          <img src={fuelImg} alt="fuel-icon" />
           <p>Save on fuel</p>
         </div>
-        <div className="benefit4">
+        <div className="benefits-item">
+          <img src={enviornmentImg} alt="enviornment-icon" />
           <p>Better for the environment</p>
         </div>
-        <div className="benefit5">
+        <div className="benefits-item">
+          <img src={chargingImg} alt="charging-icon" />
           <p>Self-charging vehicles - no charging station needed</p>
         </div>
       </div>
-      <a href="/">
-        <button className="btn benefit-btn">START NOW</button>
-      </a>
+      <div className="benefits-button">
+        <a href="/">
+          <button className="btn benefit-btn">START NOW</button>
+        </a>
+      </div>
     </section>
   );
 }
