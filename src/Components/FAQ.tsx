8a9806cb74161187fe6dbcc7ad9be17ac7f79117
gcltukjs2022 <gcltukjs2022@gmail.com>
2023-02-13 03:11:46 +0000

    modify margin and padding

--- src/Components/FAQ.tsx
+++ src/Components/FAQ.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function FAQ() {
   return (
     <section className="faq">
@@ -8,7 +6,12 @@ export default function FAQ() {
       </div>
       <div className="faq-container">
         <div className="faq-item">
-          <p>1. How long is the term?</p>
+          <div className="faq-item-question">
+            <p>1. How long is the term?</p>
+          </div>
+          <div className="faq-item-icon">
+            <p>+</p>
+          </div>
           <div className="faq-item-hidden">
             <p>
               Our Rent to Buy scheme is 54 months (4.5 years), and then you have
@@ -18,7 +21,12 @@ export default function FAQ() {
           </div>
         </div>
         <div className="faq-item">
-          <p>2. How long does it take to get a car?</p>
+          <div className="faq-item-question">
+            <p>2. How long does it take to get a car?</p>
+          </div>
+          <div className="faq-item-icon">
+            <p>+</p>
+          </div>
           <div className="faq-item-hidden">
             <p>
               Our aim is to provide you a vehicle within 14 days. Vehicles are
@@ -27,13 +35,23 @@ export default function FAQ() {
           </div>
         </div>
         <div className="faq-item">
-          <p>3. How much will it cost to rent a taxi?</p>
+          <div className="faq-item-question">
+            <p>3. How much will it cost to rent a taxi?</p>
+          </div>
+          <div className="faq-item-icon">
+            <p>+</p>
+          </div>
           <div className="faq-item-hidden">
             <p>We charge a rate of £230 per week for rental of our vehicles.</p>
           </div>
         </div>
         <div className="faq-item">
-          <p>4. What happens if I want to return my vehicle?</p>
+          <div className="faq-item-question">
+            <p>4. What happens if I want to return my vehicle?</p>
+          </div>
+          <div className="faq-item-icon">
+            <p>+</p>
+          </div>
           <div className="faq-item-hidden">
             <p>
               No problem at all! Just give us four weeks notice and then return
@@ -43,7 +61,12 @@ export default function FAQ() {
           </div>
         </div>
         <div className="faq-item">
-          <p>5. Will I own the PCO car at the end of the payment plan?</p>
+          <div className="faq-item-question">
+            <p>5. Will I own the PCO car at the end of the payment plan?</p>
+          </div>
+          <div className="faq-item-icon">
+            <p>+</p>
+          </div>
           <div className="faq-item-hidden">
             <p>
               Yes! Once you have rented the vehicle for 54 months, you have the
@@ -53,7 +76,12 @@ export default function FAQ() {
           </div>
         </div>
         <div className="faq-item">
-          <p>6. Is insurance included?</p>
+          <div className="faq-item-question">
+            <p>6. Is insurance included?</p>
+          </div>
+          <div className="faq-item-icon">
+            <p>+</p>
+          </div>
           <div className="faq-item-hidden">
             <p>
               Sorry! This is the only thing we don’t provide. Drivers will be
