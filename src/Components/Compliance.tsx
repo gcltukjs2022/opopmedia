d6b18eb814f23d0af8c70f01f06696512890301c
gcltukjs2022 <gcltukjs2022@gmail.com>
2023-02-19 18:41:02 +0000

    modify hiw and compliance layout

--- src/Components/Compliance.tsx
+++ src/Components/Compliance.tsx
@@ -1,20 +1,25 @@
-import React from "react";
-
 export default function Compliance() {
   return (
     <section className="compliance">
-      <div className="compliance-main">
-        <h1>Why does my taxi need to be compliant?</h1>
-        <p>
+      <div className="compliance-container">
+        <div className="compliance-title">
+          <h1>Why does my taxi need to be compliant?</h1>
+        </div>
+        <div className="compliance-main">
           Clean air zones across the UK aim to reduce the number of polluting
-          vehicles on the nation's roads. Any taxi that fails to meet emissions
-          standards must pay a daily charge to drive anywhere in major cities
-          such as London and Manchester. You won’t be charged in these zones
-          with any Boomerang vehicle.
-        </p>
-        <a href="/">
-          <button className="btn-start-blue">START NOW</button>
-        </a>
+          vehicles on the nation's roads.
+          <p>
+            Any taxi that fails to meet emissions standards must pay a daily
+            charge to drive anywhere in major cities such as London and
+            Manchester.
+          </p>
+          <p>You won’t be charged in these zones with any Boomerang vehicle.</p>
+        </div>
+        <div className="compliance-btn">
+          <a href="/">
+            <button className="btn-start-blue">START NOW</button>
+          </a>
+        </div>
       </div>
     </section>
   );
