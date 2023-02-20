cf4a5e6caa1766312e81b05e03722d68dbde1bb9
gcltukjs2022 <gcltukjs2022@gmail.com>
2023-02-20 05:37:37 +0000

    fix css warning

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import "./css/App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./Components/Home";
-import Contact from "./Components/Contact";
 
 function App() {
   return (
@@ -9,7 +8,6 @@ function App() {
       <main className="main">
         <Routes>
           <Route path="/" element={<Home />}></Route>
-          <Route path="contact" element={<Contact />}></Route>
         </Routes>
       </main>
     </div>
